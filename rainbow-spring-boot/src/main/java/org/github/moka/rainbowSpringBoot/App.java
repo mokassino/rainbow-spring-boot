39d6b74798e8e23423b04a70a2e4eedd2cc5d8a7
mokassino <m000ka@proton.me>
2023-05-09 21:33:38 +0200

    created mvn basic project

---
+++ rainbow-spring-boot/src/main/java/org/github/moka/rainbowSpringBoot/App.java
@@ -0,0 +1,13 @@
+package org.github.moka.rainbowSpringBoot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Ciao mondo, quanto è complicato fare anche la cosa più semplice in Java" );
+    }
+}
